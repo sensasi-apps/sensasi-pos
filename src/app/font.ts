0f9ebf9932cdbacd402f060ee136cacf14c2a976
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-09-02 18:33:02 +0700

    Migrasi antarmuka pengguna dari MUI ke NextUI (#19)

---
+++ src/app/font.ts
@@ -0,0 +1,7 @@
+import { Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "700"],
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,27 @@
-import { Alert, Box, Button, Container, Typography } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { Button } from "@nextui-org/react";
+import { Github, TriangleAlert } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
-	return (
-		<Container
-			component="main"
-			maxWidth="sm"
-			sx={{
-				height: "100dvh",
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "center",
-				alignItems: "center",
-				gap: 2,
-			}}
-		>
-			<Typography variant="h2" component="h1" fontWeight="bold">
-				Sensasi POS
-			</Typography>
-
-			<Alert severity="warning" variant="filled">
-				Aplikasi masih dalam tahap pengembangan.
-			</Alert>
-
-			<Typography variant="body1" component="p">
-				Sensasi POS adalah aplikasi Point of Sale (POS) berbasis web yang
-				dirancang untuk membantu Anda mengelola transaksi bisnis dengan mudah
-				dan efisien.
-			</Typography>
-
-			<Button
-				startIcon={<GitHubIcon fontSize="inherit" />}
-				size="small"
-				href="https://github.com/sensasi-apps/sensasi-pos"
-				target="_blank"
-			>
-				GitHub
-			</Button>
-		</Container>
-	);
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center max-w-xl gap-6">
+        <h1 className="text-7xl font-bold">Sensasi POS</h1>
+        <p className="flex flex-row p-4 bg-orange-500 rounded ">
+          <TriangleAlert className="mx-2" />
+          Aplikasi masih dalam tahap pengembangan
+        </p>
+        <p>
+          Sensasi POS adalah aplikasi Point of Sale (POS) berbasis web yang dirancang untuk membantu Anda mengelola transaksi bisnis dengan mudah dan
+          efisien.
+        </p>
+        <Button variant="light" color="primary">
+          <Link href="https://github.com/sensasi-apps/sensasi-pos" className="flex flex-row justify-center items-center">
+            <Github className="mr-2" />
+            Github
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
