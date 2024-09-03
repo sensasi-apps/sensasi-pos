6ebd71fc768f55e7b10b56545f5bf5cc5b6ed8b8
Zain Adam <zainadam.id@gmail.com>
2024-09-04 00:10:17 +0800

    Menyempurnakan tata letak dan menambahkan komponen-komponen penyaring

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,19 @@
 import type Product from '@/@types/data/product'
+
 import ProductCard from '@/components/product-card'
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  Chip,
+  Input,
+  Select,
+  SelectItem,
+} from '@nextui-org/react'
+
+import { Barcode, LayoutGridIcon, List, PlusCircle, Search } from 'lucide-react'
+import Link from 'next/link'
 
 const dummyProducts: Product[] = [
   {
@@ -67,11 +81,83 @@ const dummyProducts: Product[] = [
 export default function ProductsPage() {
   return (
     <>
-      <ul className="list-none flex flex-col gap-4">
-        {dummyProducts.map((product, i) => (
-          <ProductCard key={i} data={product} as="li" />
-        ))}
-      </ul>
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-4 max-md:col-span-12 flex items-center mb-4">
+          <h1 className="text-2xl font-bold">Produk</h1>
+          <Chip className="ml-4" size="sm">
+            100 <i>item</i>
+          </Chip>
+        </div>
+
+        <div className="col-span-8 flex max-md:col-span-12 items-center mb-4 gap-6">
+          <Input
+            className="flex-1"
+            variant="bordered"
+            placeholder="Cari Produk"
+            startContent={<Search className="text-default-400" />}
+            endContent={
+              <Button
+                isIconOnly
+                aria-label="Pindai kode batang"
+                variant="light">
+                <Barcode className="text-2xl text-primary font-bold pointer-events-none" />
+              </Button>
+            }
+          />
+
+          <ButtonGroup>
+            <Button isIconOnly variant="solid" color="primary">
+              <List className="text-2xl text-default-400 pointer-events-none" />
+            </Button>
+            <Button isIconOnly variant="light">
+              <LayoutGridIcon className="text-2xl text-default-400 pointer-events-none" />
+            </Button>
+          </ButtonGroup>
+
+          <Button
+            variant="flat"
+            color="success"
+            startContent={<PlusCircle />}
+            as={Link}
+            href="">
+            Tambah Produk
+          </Button>
+        </div>
+      </div>
+
+      <p className="text-default-400 text-xs text-right mb-2">
+        Menampilkan 10 dari 124 <i>item</i>
+      </p>
+
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-4">
+          <Card shadow="sm">
+            <CardBody>
+              <h2 className="text-lg font-bold mb-4">Penyaring</h2>
+
+              <Select
+                label="Kategori"
+                placeholder="Pilih satu atau lebih"
+                selectionMode="multiple">
+                <SelectItem key="1">aswda</SelectItem>
+                <SelectItem key="2">aswda</SelectItem>
+                <SelectItem key="3">aswda</SelectItem>
+                <SelectItem key="4">aswda</SelectItem>
+                <SelectItem key="5">aswda</SelectItem>
+                <SelectItem key="6">aswda</SelectItem>
+              </Select>
+            </CardBody>
+          </Card>
+        </div>
+
+        <div className="col-span-8">
+          <ul className="list-none flex flex-col gap-4">
+            {dummyProducts.map((product, i) => (
+              <ProductCard key={i} data={product} as="li" />
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
