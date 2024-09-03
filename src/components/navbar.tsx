718aee12ddfd335b95dcb3509870156e420485e1
Adam Akbar <zainadam.id@gmail.com>
2024-09-04 04:59:38 +0800

    Menambahkan tombol navigasi ke halaman lain dan penataan ulang komponen (#33)
    * Mengganti ukuran ke `sm` dan menghilangkan margin
    
    * Menambahkan tombol navigasi ke halaman lain dan penataan ulang
    
    * wip: Mengganti nama file sementara untuk mengikuti standar nama file
    
    * Mengganti nama file untuk mengikuti standar penamaan file

---
+++ src/components/navbar.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import {
+  Navbar as VendorNavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@nextui-org/react'
+import NextLink from 'next/link'
+import { ComputerIcon, FileSpreadsheetIcon } from 'lucide-react'
+import ThemeSwitcher from './theme-switcher'
+import SettingsDropdownButtonInNavbar from './settings-dropdown-button-in-navbar'
+
+/**
+ *
+ * @todo Implementasi render kondisi terautentikasi
+ */
+export default function Navbar() {
+  return (
+    <VendorNavbar>
+      <NavbarBrand
+        as={NextLink}
+        href="/"
+        className="flex items-end text-end hover:text-primary-300 duration-150">
+        <ComputerIcon size={24} className="text-primary-300" />
+        <p className="font-bold text-inherit ml-2 leading-4">Sensasi POS</p>
+      </NavbarBrand>
+
+      <NavbarContent justify="end">
+        {/* guest */}
+        <NavbarItem>
+          <Link href="/login" as={NextLink}>
+            Login
+          </Link>
+        </NavbarItem>
+
+        <NavbarItem>
+          <ThemeSwitcher />
+        </NavbarItem>
+
+        {/* auth */}
+        <NavbarItem>
+          <Button
+            href="/dashboard/reports"
+            as={NextLink}
+            variant="shadow"
+            color="primary"
+            startContent={<FileSpreadsheetIcon />}>
+            Laporan
+          </Button>
+        </NavbarItem>
+
+        <NavbarItem>
+          <SettingsDropdownButtonInNavbar />
+        </NavbarItem>
+      </NavbarContent>
+    </VendorNavbar>
+  )
+}
--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,22 +1,24 @@
 import { useTheme } from 'next-themes'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Switch } from '@nextui-org/switch'
 import { MoonStar, Sun } from 'lucide-react'
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = React.useState(false)
+  const [mounted, setMounted] = useState(false)
 
-  React.useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), [])
 
   if (!mounted) return null
 
   return (
     <Switch
-      defaultSelected
-      defaultChecked={theme === 'dark'}
+      classNames={{
+        wrapper: 'm-0',
+      }}
+      defaultSelected={theme === 'dark'}
       onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      size="md"
+      size="sm"
       color="primary"
       startContent={<Sun />}
       endContent={<MoonStar />}
