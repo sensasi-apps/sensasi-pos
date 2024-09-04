5f422040496021338c9cd156055daca080d945b4
Zain Adam <zainadam.id@gmail.com>
2024-09-05 01:56:01 +0800

    Menambahkan durasi transisi pada pergantian warna latar belakang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="transition-[background-color] duration-1000 ease-in-out">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
