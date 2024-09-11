079f2c6375ab14f41200b7a9a067f64b417538bf
Zain Adam <zainadam.id@gmail.com>
2024-09-12 03:55:00 +0800

    Memperbaiki halaman dengan `onClick` seharusnya menjadi komponen klien

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -1,13 +1,19 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+
 export default function SentryExamplePage() {
   return (
     <div>
-      <h1>Sentry Example Page</h1>
-      <button
+      <h1 className="text-3xl font-bold mb-4">Sentry Example Page</h1>
+
+      <Button
+        color="danger"
         onClick={() => {
           throw new Error('Sentry Example Page Error')
         }}>
         Throw Error
-      </button>
+      </Button>
     </div>
   )
 }
