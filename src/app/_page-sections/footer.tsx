9c48fb88a50b55108ec26c824a23ee28ad1d3b9e
Adam Akbar <zainadam.id@gmail.com>
2024-09-16 03:50:27 +0800

    Menyempurnakan komponen `<Footer />` (#79)
    menyertakan versi dan restruktur html

--- src/app/_page-sections/footer.tsx
+++ src/app/_page-sections/footer.tsx
@@ -1,47 +1,72 @@
 import { Link } from '@nextui-org/react'
-import { ArrowUpRight } from 'lucide-react'
+import { ArrowUpRightIcon, ComputerIcon } from 'lucide-react'
+import packageJson from '@/../package.json'
 
 export function Footer() {
   return (
-    <footer className="bg-gray-800 text-gray-400 p-8 grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
-      <p>
-        Sensasi POS adalah aplikasi Point of Sale sederhana yang dirancang untuk
-        membantu pencatatan penjualan barang pada Warung / Toko / UMKM / Stan /
-        Gerai / Swalayan.
-      </p>
-
-      <div>
+    <footer className="p-10 bg-gray-800 text-gray-400 text-sm text-balance">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <p>
-          Sensasi POS bersifat <i>open-source</i> dan dapat dikembangkan oleh
-          siapa saja termasuk Anda. Jika Anda tertarik, Silakan lihat{' '}
-          <Link
-            size="sm"
-            href="https://github.com/sensasi-apps/sensasi-pos/CONTRIBUTING.md"
-            target="_blank">
-            tata cara kontribusi <ArrowUpRight className="max-w-[1rem]" />
-          </Link>{' '}
-          pada{' '}
-          <Link
-            size="sm"
-            href="https://github.com/sensasi-apps/sensasi-pos"
-            target="_blank">
-            halaman GitHub kami <ArrowUpRight className="max-w-[1rem]" />
-          </Link>
-          .
+          Sensasi POS adalah aplikasi Point of Sale sederhana yang dirancang
+          untuk membantu pencatatan penjualan barang pada Warung / Toko / UMKM /
+          Stan / Gerai / Swalayan.
         </p>
 
-        <p className="text-xs mt-4 text-gray-500">
-          Panjang umur untuk semua pengembang <i>open-source</i> di seluruh
-          dunia 🌍
+        <div>
+          <p>
+            Sensasi POS bersifat <i>open-source</i> dan dapat dikembangkan oleh
+            siapa saja termasuk Anda. Jika Anda tertarik, Silakan lihat{' '}
+            <Link
+              size="sm"
+              href="https://github.com/sensasi-apps/sensasi-pos/blob/main/CONTRIBUTING.md"
+              target="_blank">
+              tata cara kontribusi <ArrowUpRightIcon className="max-w-[1rem]" />
+            </Link>{' '}
+            pada{' '}
+            <Link
+              size="sm"
+              href="https://github.com/sensasi-apps/sensasi-pos"
+              target="_blank">
+              halaman GitHub kami <ArrowUpRightIcon className="max-w-[1rem]" />
+            </Link>
+            .
+          </p>
+        </div>
+
+        <p>
+          Masih memiliki pertanyaan? silahkan mengirimkannya melalui surel ke:{' '}
+          <Link size="sm" href="mailto:sensasi.apps@gmail.com" target="_blank">
+            sensasi.apps@gmail.com <ArrowUpRightIcon className="max-w-[1rem]" />
+          </Link>
         </p>
       </div>
 
-      <p>
-        Masih memiliki pertanyaan? silahkan mengirimkannya melalui surel ke:
-        <Link size="sm" href="mailto:sensasi.apps@gmail.com" target="_blank">
-          sensasi.apps@gmail.com <ArrowUpRight className="max-w-[1rem]" />
-        </Link>
-      </p>
+      <div className="text-xs mt-20 text-gray-500 flex flex-row max-md:flex-col md:gap-3 md:items-center max-lg:text-center">
+        <div className="flex items-center max-md:items-end gap-1 justify-center leading-4">
+          <ComputerIcon className="max-h-full text-primary" /> Sensasi POS v
+          {packageJson.version} ({packageJson.versionDate})
+        </div>
+
+        <div className="max-md:hidden">•</div>
+
+        <div>
+          Dibuat dengan ❤️ oleh{' '}
+          <Link
+            className="text-xs"
+            href="https://github.com/sensasi-apps"
+            target="_blank">
+            Sensasi Apps🍕
+            <ArrowUpRightIcon className="max-w-[1rem]" />
+          </Link>
+        </div>
+
+        <div className="max-md:hidden">•</div>
+
+        <div>
+          Panjang umur untuk semua pengembang <i>open-source</i> di seluruh
+          dunia 🌍
+        </div>
+      </div>
     </footer>
   )
 }
