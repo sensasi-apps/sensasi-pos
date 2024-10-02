aef5c6ffda0c49b98644d854a7a1fa5d591b2eb7
Zain Adam <zainadam.id@gmail.com>
2024-10-03 03:21:08 +0800

    Inisialisasi rute-rute halaman laporan (#90 #94 #92 #91)

--- src/app/(authenticated user)/reports/page.tsx
+++ src/app/(authenticated user)/reports/page.tsx
@@ -1,7 +1,41 @@
+import PageUrlEnum from '@/enums/page-url'
+import { Button, Link } from '@nextui-org/react'
+import NextLink from 'next/link'
+
 export default function page() {
   return (
     <div>
       <div>(Halaman Daftar Laporan)</div>
+
+      <ul>
+        <li>
+          <Link href={PageUrlEnum.REPORT_SALE_PER_PRODUCT} as={NextLink}>
+            Penjualan per produk
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href={PageUrlEnum.REPORT_SALE_PER_PRODUCT_CATEGORY}
+            as={NextLink}>
+            Penjualan per kategori produk
+          </Link>
+        </li>
+
+        <li>
+          <Link href={PageUrlEnum.REPORT_SALE_PER_TX} as={NextLink}>
+            Penjualan per transaksi
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href={PageUrlEnum.REPORT_STOCK_IN_OUT_PER_PRODUCT}
+            as={NextLink}>
+            Stok masuk/keluar per produk
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
---
+++ src/app/(authenticated user)/reports/sale-per-tx/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <div>(Halaman Laporan Penjualan per Transaksi)</div>
+    </div>
+  )
+}
