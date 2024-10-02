b60f968dd8c70eef41989f2b1d9d4955626a2da5
Zain Adam <zainadam.id@gmail.com>
2024-10-03 03:08:54 +0800

    Inisialisasi rute-rute halaman autentikasi (#85 #89 #124)

---
+++ src/app/(guest)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div>(Halaman Lupa Kata Sandi)</div>
+    </div>
+  )
+}
---
+++ src/app/(guest)/reset-password/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div>(Halaman Reset Kata Sandi)</div>
+    </div>
+  )
+}
