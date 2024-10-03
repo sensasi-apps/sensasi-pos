30532ca1e1c8a50f7966ba7ff2b6578535aeb630
Adam Akbar <zainadam.id@gmail.com>
2024-10-03 18:49:37 +0800

    Inisialisasi rute-rute halaman onboarding (#140)

---
+++ src/app/(guest)/onboarding/steps/1-add-admin-user/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import PageUrlEnum from '@/enums/page-url'
+import { Button } from '@nextui-org/react'
+import { MoveRightIcon } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+export default function Page() {
+  const router = useRouter()
+
+  return (
+    <div>
+      <div>(Halaman untuk mendaftarkan admin (manajer))</div>
+
+      <Button variant="light" onClick={() => router.back()}>
+        Kembali
+      </Button>
+
+      <Button
+        variant="flat"
+        endContent={<MoveRightIcon />}
+        href={PageUrlEnum.ONBOARDING_STEP_2}
+        as={Link}>
+        Selanjunya
+      </Button>
+    </div>
+  )
+}
---
+++ src/app/(guest)/onboarding/steps/finish/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import PageUrlEnum from '@/enums/page-url'
+import { Button } from '@nextui-org/react'
+import { MoveRightIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <div>
+      <div>(Halaman akhir dari proses onboarding)</div>
+
+      <div>Sensasi POS siap digunakan!</div>
+
+      <Button
+        variant="flat"
+        endContent={<MoveRightIcon />}
+        href={PageUrlEnum.DASHBOARD}
+        as={Link}>
+        Lihat Dasbor
+      </Button>
+    </div>
+  )
+}
