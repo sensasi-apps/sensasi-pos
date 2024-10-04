d85a6a87812e498ef59926040a4d6ca3bb7df4c6
Theo Zebua <87429668+theozebua@users.noreply.github.com>
2024-10-04 22:25:19 +0700

    Membangun Antarmuka Halaman Lupa Kata Sandi (#168)

--- src/app/(guest)/forgot-password/page.tsx
+++ src/app/(guest)/forgot-password/page.tsx
@@ -1,7 +1,41 @@
+'use client'
+
+import { Card, CardBody, CardHeader, Divider } from '@nextui-org/react'
+import EmailForm from './_components/email-form'
+import SecurityQuestionForm from './_components/security-question'
+import Link from 'next/link'
+import PageUrlEnum from '@/enums/page-url'
+import { useSearchParams } from 'next/navigation'
+
 export default function Page() {
+  const query = useSearchParams()
+  const selectedMethod = query.get('method')
+
   return (
-    <div>
-      <div>(Halaman Lupa Kata Sandi)</div>
+    <div className="container mt-8">
+      <div className="flex justify-center">
+        <Card className="max-w-md md:min-w-[400px]">
+          <CardHeader>
+            <span className="mx-auto block text-xl font-bold">
+              Lupa Kata Sandi
+            </span>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            {selectedMethod === 'security-question' ? (
+              <SecurityQuestionForm />
+            ) : (
+              <EmailForm />
+            )}
+
+            <Link
+              className="mt-4 block cursor-pointer text-center text-sm outline-none"
+              href={`${PageUrlEnum.FORGOT_PASSWORD}?method=${selectedMethod === 'security-question' ? 'email' : 'security-question'}`}>
+              Coba Cara Lain?
+            </Link>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   )
 }
