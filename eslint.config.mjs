925e368b0a11fcf93fc4636869bd4c49ebc07944
sensasi-delight <zainadam.id@gmail.com>
2025-10-03 02:04:34 +0800

    Menambahkan aturan penamaan file kebab-case (#205)
    Mengintegrasikan `eslint-plugin-unicorn` untuk menerapkan konvensi penamaan file kebab-case. Ini membantu menjaga konsistensi dan keterbacaan kode dalam proyek.
    
    Aturan ini dilengkapi pengecualian untuk pola penamaan file khusus Next.js, seperti segmen dinamis, grup rute, dan file yang diawali garis bawah.
    
    fix #202

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import unicorn from 'eslint-plugin-unicorn'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -21,7 +22,32 @@ const eslintConfig = [
       'next-env.d.ts',
     ],
   },
+  {
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+          ignore: [
+            // This regex matches dynamic segment files
+            // like `[id].tsx` or `[...slug].tsx`.
+            /^\[.*\]\..+$/,
 
+            // This is for the uncommon case where a file is named like a route group,
+            // e.g., `(marketing).tsx`.
+            /^\(.*\)\..+$/,
+
+            // This correctly ignores files that start with an underscore,
+            // such as `_app.tsx` or private utility files.
+            /^_/,
+          ],
+        },
+      ],
+    },
+  },
   eslintPluginPrettierRecommended,
 ]
 
