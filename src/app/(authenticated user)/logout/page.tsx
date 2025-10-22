f935372900d87d2f9412f003b976ef40952c1e74
Zain🍕 <zainadam.id@gmail.com>
2025-10-22 14:05:17 +0800

    Memastikan reset status logout berjalan sebagai efek.
    Memindahkan pemanggilan `setLoggedInUser(undefined)` ke dalam `useEffectEvent`. Hal ini mencegah pembaruan status pengguna berjalan di setiap render, memastikan tindakan logout ditangani sebagai efek samping yang terkontrol dan meningkatkan pengelolaan siklus hidup komponen.

--- src/app/(authenticated user)/logout/page.tsx
+++ src/app/(authenticated user)/logout/page.tsx
@@ -2,11 +2,14 @@
 
 import useAuth from '@/hooks/use-auth'
 import { Spinner } from '@heroui/spinner'
+import { useEffectEvent } from 'react'
 
 export default function Page() {
   const { setLoggedInUser } = useAuth()
 
-  setLoggedInUser(undefined)
+  useEffectEvent(() => {
+    setLoggedInUser(undefined)
+  })
 
   return <Spinner size="lg" />
 }
