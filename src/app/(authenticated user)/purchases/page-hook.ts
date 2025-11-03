aff23f80fd3bffc89dd3d62643640acae6ca5e3a
Ya Sudahlah <168106387+IRTIDEATH@users.noreply.github.com>
2025-11-03 21:41:56 +0700

    Membuat fitur pencarian pada halaman pengadaan (#228)
    * Membuat fitur pencarian pada halaman pengadaan
    
    * Terapkan pelacakan pengguna & perbaiki pencarian
    
    Merekam pengguna yang membuat dan menghapus data pengadaan untuk auditabilitas yang lebih baik.
    Mengintegrasikan hook `useAuth` untuk menyimpan detail pengguna pada entri `by_user_state` dan `deleted_by_user_state`.
    
    Memperbaiki fungsionalitas pencarian pada daftar pengadaan.
    Memindahkan logika kueri Dexie ke fungsi `createQuery` terpisah untuk keterbacaan yang lebih baik.
    Pencarian kini juga mencakup filter berdasarkan informasi pengguna terkait (nama, email, UUID), di samping referensi, catatan, dan detail produk.
    
    * fix lint error
    
    * Memperbaiki filter pencarian pembelian
    
    Memastikan parameter pencarian ('q') selalu diterapkan saat mengambil daftar pergerakan produk dan jumlah totalnya. Ini memperbaiki fungsionalitas pencarian agar bekerja konsisten di halaman pembelian, dengan membuat parameter 'q' wajib di fungsi kueri.
    
    ---------
    
    Co-authored-by: Zain🍕 <zainadam.id@gmail.com>

--- src/app/(authenticated user)/purchases/page-hook.ts
+++ src/app/(authenticated user)/purchases/page-hook.ts
@@ -2,35 +2,49 @@ import { toast } from '@/functions/toast'
 import db from '@/models/db'
 import type { ProductMovement } from '@/models/table-types/product-movement'
 import { useLiveQuery } from 'dexie-react-hooks'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useDebounce } from '@/hooks/use-debounce'
+import useAuth from '@/hooks/use-auth'
 
 export function usePageHook({
   page,
   rowsPerPage,
+  q,
 }: {
   page: number
   rowsPerPage: number
+  q: string
 }) {
+  const { user } = useAuth()
   const [toBeDeletedProductMovement, setToBeDeletedProductMovement] =
     useState<ProductMovement>()
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const [search, setSearch] = useState(q)
+  const debouncedSearch = useDebounce(search, 300)
+
+  useEffect(() => {
+    if (q !== debouncedSearch) {
+      const params = new URLSearchParams(searchParams)
+      params.set('q', debouncedSearch)
+      params.set('page', '1')
+      router.push(`${pathname}?${params.toString()}`)
+    }
+  }, [debouncedSearch, q, searchParams, pathname, router])
 
   const productMovements = useLiveQuery(
     () =>
-      db.productMovements
-        .orderBy('created_at')
-        .reverse()
-        .filter(movement => movement.type === 'purchase')
+      createQuery(q)
         .offset((page - 1) * rowsPerPage)
         .limit(rowsPerPage)
         .toArray(),
-    [page, rowsPerPage],
+    [page, rowsPerPage, q],
   )
 
-  const totalProductMovements = useLiveQuery(() =>
-    db.productMovements
-      .filter(movement => movement.type === 'purchase')
-      .count(),
-  )
+  const totalProductMovements = useLiveQuery(() => createQuery(q).count(), [q])
 
   const handleDeleteProductMovement = () => {
     if (!toBeDeletedProductMovement) return
@@ -38,7 +52,7 @@ export function usePageHook({
     db.productMovements
       .update(toBeDeletedProductMovement.uuid, {
         deleted_at: new Date().toISOString(),
-        // deleted_by_user_state: getAuthUser(),
+        deleted_by_user_state: user,
       })
       .then(() => {
         setToBeDeletedProductMovement(undefined)
@@ -55,5 +69,39 @@ export function usePageHook({
     toBeDeletedProductMovement,
     setToBeDeletedProductMovement,
     handleDeleteProductMovement,
+    search,
+    setSearch,
   }
+}
+
+function createQuery(q: string) {
+  const query = db.productMovements
+    .orderBy('created_at')
+    .reverse()
+    .filter(movement => movement.type === 'purchase')
+
+  if (q) {
+    query.and(({ ref, note, by_user_state, items }) => {
+      const lowerCaseQuery = q.toLowerCase()
+
+      return (
+        ref?.toLowerCase().includes(lowerCaseQuery) ||
+        note?.toLowerCase().includes(lowerCaseQuery) ||
+        //
+        items.some(
+          ({ product_state: { name, uuid } }) =>
+            name?.toLowerCase().includes(lowerCaseQuery) ||
+            uuid?.toLowerCase().includes(lowerCaseQuery),
+        ) ||
+        //
+        by_user_state?.name
+          ?.toLowerCase()
+          .includes(lowerCaseQuery) ||
+        by_user_state?.email?.toLowerCase().includes(lowerCaseQuery) ||
+        by_user_state?.uuid?.toLowerCase().includes(lowerCaseQuery)
+      )
+    })
+  }
+
+  return query
 }
