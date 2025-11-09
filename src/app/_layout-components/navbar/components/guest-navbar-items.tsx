68789e666b8ac8151f4763d814a02d0711a8a215
sensasi-delight <zainadam.id@gmail.com>
2025-11-10 00:26:27 +0800

    Menambahkan hook useIsAppAlreadyInitialized untuk memeriksa status inisialisasi aplikasi pada komponen GuestNavbarItems dan Cta (#233)
    Implementasi hook baru untuk memeriksa apakah aplikasi sudah diinisialisasi, dan gunakan hook ini pada komponen GuestNavbarItems dan Cta untuk menyesuaikan tautan yang ditampilkan berdasarkan status inisialisasi.

--- src/app/_layout-components/navbar/components/guest-navbar-items.tsx
+++ src/app/_layout-components/navbar/components/guest-navbar-items.tsx
@@ -1,20 +1,28 @@
+'use client'
+
 // vendors
 import { NavbarItem } from '@heroui/navbar'
 import { Link } from '@heroui/link'
 // globals
 import PageUrlEnum from '@/enums/page-url'
 import ThemeSwitcher from '@/components/theme-switcher'
+import useIsAppAlreadyInitialized from '@/hooks/use-is-app-already-initialized'
 
 /**
  * Navbar items for guest users
- *
- * @todo Remove NODE_ENV below when #85 is solved
  */
 export function GuestNavbarItems() {
+  const appIsAlreadyInitialized = useIsAppAlreadyInitialized()
+
   return (
     <>
       <NavbarItem>
-        <Link href={PageUrlEnum.LOGIN}>Login</Link>
+        <Link
+          href={
+            appIsAlreadyInitialized ? PageUrlEnum.LOGIN : PageUrlEnum.ONBOARDING
+          }>
+          {appIsAlreadyInitialized ? 'Login' : 'Coba Sekarang'}
+        </Link>
       </NavbarItem>
 
       <NavbarItem>
